02c41b46e0ab3714045cd3a078e4d221ae260a32
Ray <lhy131120@gmail.com>
2025-01-24 12:16:04 +0800

    upload to main

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h2>Dashboard</h2>
+      <h3>Here</h3>
+    </>
+  )
+}
+
+export default App
